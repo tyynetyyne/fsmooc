950036f0fad3c59ef357e13d3d0e54f3b2c7dc34
Tiina Partanen <tyynetyyne@gmail.com>
2018-10-15 15:53:13 +0300

    Tehtava 1.2

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -19,12 +19,20 @@ const Otsikko = (props) => {
     )
 }
 
+const Osa = (props) => {
+  return (
+    <div>
+    <p>{props.osa} {props.tehtavia}</p>
+    </div>
+  )
+}
+
 const Sisalto = (props) => {
     return (
       <div>
-        <p>{props.osa1} {props.tehtavia1}</p>
-        <p>{props.osa2} {props.tehtavia2}</p>
-        <p>{props.osa3} {props.tehtavia3}</p>
+        <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+        <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+        <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
       </div>
     )
 }
