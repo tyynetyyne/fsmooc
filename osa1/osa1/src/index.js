b7fba04c8344dadbbe152e84a07e8ec06664fabf
Tiina Partanen <tyynetyyne@gmail.com>
2018-10-15 16:12:11 +0300

    Tehtava 1.5

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -3,21 +3,23 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
  
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osat = [
-    {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    },
-    {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    },
-    {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
-  ]
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
 
 const Otsikko = (props) => {
     return (
@@ -55,9 +57,9 @@ const Yhteensa = (props) => {
 
 return (
   <div>
-    <Otsikko kurssi={kurssi} />
-    <Sisalto osat={osat} />
-    <Yhteensa osat={osat} />
+    <Otsikko kurssi={kurssi.nimi} />
+    <Sisalto osat={kurssi.osat} />
+    <Yhteensa osat={kurssi.osat} />
   </div>
 )
 }
