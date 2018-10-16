d97692fa8c065d58f22c9961876c0cf4d58ff48a
Tiina Partanen <tyynetyyne@gmail.com>
2018-10-16 16:49:35 +0300

    tehtava 1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,79 +1,95 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hyva: 0,
-      neutraali: 0,
-      huono: 0
-    }
-  }
-
-  hyva() {
-    this.setState({ hyva: this.state.hyva + 1 })
-  }
-
-  neutraali() {
-    this.setState({ neutraali: this.state.neutraali + 1 })
-  }
-
-  huono() {
-    this.setState({ huono: this.state.huono + 1 })
-  }
-
- kaikki() {
-    return this.state.hyva + this.state.neutraali + this.state.huono
- }
- 
- pyorista(luku){
-     return Math.round(luku * 100) / 100
- }
-
- keskiarvo() {
-    if(this.kaikki() === 0)
-    return 0
-    return this.pyorista((this.state.hyva - this.state.huono) / this.kaikki())
-  }
-
-positiivisia() {
-    if(this.kaikki() === 0)
-    return 0
-    return this.pyorista(this.state.hyva / this.kaikki())
-  }
-
-  render() {
+function Statistic(props) {
     return (
-      <div>
-        <div>
-        <h1>Anna palautetta</h1>
-          <button onClick={this.hyva.bind(this)}>
-            Hyvä
-          </button>
-          <button onClick={this.neutraali.bind(this)}>
-            Neutraali
-          </button>
-          <button onClick={this.huono.bind(this)}>
-            Huono
-          </button>
-        </div>
-        <div>
-        <h1>Statistiikka</h1>
-        <ul>
-            <li>Hyvä: {this.state.hyva}</li>
-            <li>Neutraali: {this.state.neutraali}</li> 
-            <li>Huono: {this.state.huono}</li>
-            <li>Keskiarvo: {this.keskiarvo()} </li>
-            <li>Positiivisia: {this.positiivisia()} % </li>
-        </ul>
-        </div>
-      </div>
+        <li>{props.teksti}: {props.arvo} {props.yksikko}</li>
     )
-  }
+}
+
+function Statistics(props) {
+    return (
+        <div>
+            <h1>Statistiikka</h1>
+            <ul>
+                <Statistic teksti="Hyvä" arvo={props.hyva} />
+                <Statistic teksti="Neutraali" arvo={props.neutraali} />
+                <Statistic teksti="Huono" arvo={props.huono} />
+                <Statistic teksti="Keskiarvo" arvo={props.keskiarvo} />
+                <Statistic teksti="Positiivisia" arvo={props.positiivisia} yksikko="%" />
+            </ul>
+        </div>
+    )
+}
+
+function Button(props){
+    return (
+    <button onClick={props.hoidaPainallus}>
+        {props.teksti}
+    </button>
+    )
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hyva: 0,
+            neutraali: 0,
+            huono: 0
+        }
+    }
+
+    hyva() {
+        this.setState({ hyva: this.state.hyva + 1 })
+    }
+
+    neutraali() {
+        this.setState({ neutraali: this.state.neutraali + 1 })
+    }
+
+    huono() {
+        this.setState({ huono: this.state.huono + 1 })
+    }
+
+    kaikki() {
+        return this.state.hyva + this.state.neutraali + this.state.huono
+    }
+
+    pyorista(luku) {
+        return Math.round(luku * 100) / 100
+    }
+
+    keskiarvo() {
+        if (this.kaikki() === 0)
+            return 0
+        return this.pyorista((this.state.hyva - this.state.huono) / this.kaikki())
+    }
+
+    positiivisia() {
+        if (this.kaikki() === 0)
+            return 0
+        return this.pyorista(this.state.hyva / this.kaikki())
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Anna palautetta</h1>
+                    <Button teksti="Hyvä" hoidaPainallus={this.hyva.bind(this)} />
+                    <Button teksti="Neutraali" hoidaPainallus={this.neutraali.bind(this)} />
+                    <Button teksti="Huono" hoidaPainallus={this.huono.bind(this)} />
+                </div>
+                <div>
+                    <Statistics hyva={this.state.hyva} neutraali={this.state.neutraali} huono={this.state.huono} keskiarvo={this.keskiarvo()} positiivisia={this.state.hyva} />
+                </div>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 )
