a4d1a103e0f76cd7e32a02d24fff553fd805da8e
Tiina Partanen <tyynetyyne@gmail.com>
2018-10-18 15:57:42 +0300

    Tehtava 2.11

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 
 const Person = (props) => {
     return (
@@ -57,16 +58,22 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            persons: [
-                { name: 'Arto Hellas', number: '050-3429322' },
-                { name: 'Lea Kutvo', number: '050-3420320' }
-            ],
+            persons: [],
             newName: '',
             newNumber: '',
             newFilter: ''
         }
     }
 
+    componentDidMount() {
+        console.log('did mount')
+        axios
+          .get('http://localhost:3001/persons')
+          .then(response => {
+            console.log('promise fulfilled')
+            this.setState({ persons: response.data })
+          })
+      }
 
     addPerson = (event) => {
         event.preventDefault();
