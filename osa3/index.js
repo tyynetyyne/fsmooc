d19f7f970271e7f31bdc02fcaf4338212fe423d7
Tiina Partanen <tyynetyyne@gmail.com>
2018-10-28 15:20:26 +0200

    Tehtavä 3.2

---
+++ osa3/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      name: "Arto Hellas",
+      number: "040-123456",
+      id: 1
+    },
+    {
+      name: "Martti Tienari",
+      number: "040-123456",
+      id: 2
+    },
+    {
+      name: "Arto Järvinen",
+      number: "040-123456",
+      id: 3
+    },
+    {
+      name: "Lea Kutvonen",
+      number: "040-123456",
+      id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/info', (req, res) => {
+    console.log('time', Date.UTC())
+    const now = new Date()
+    res.send(`<p>puhelinluettelossa ${persons.length} henkilön tiedot</p><br/> ${now.toString()}`)
+  })
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
