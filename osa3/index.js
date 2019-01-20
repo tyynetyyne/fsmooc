412c97956203ab348b732b62f2b90de5e3e187f6
Tiina Partanen <tyynetyyne@gmail.com>
2019-01-20 18:39:45 +0200

    Pipeline

--- osa3/index.js
+++ osa3/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
@@ -9,32 +8,37 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
 //app.use(morgan('tiny'))
-morgan.token('myname', function (req) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :myname :status :res[content-length] - :response-time ms'))
+morgan.token('myname', function(req) {
+  return JSON.stringify(req.body)
+})
+app.use(
+  morgan(
+    ':method :url :myname :status :res[content-length] - :response-time ms'
+  )
+)
 
 app.get('/', (request, response) => {
-  response.send('<h1>Puhelinluettelo</h1><p>/info, /api/persons, /api/persons/:id</p>')
+  response.send(
+    '<h1>Puhelinluettelo</h1><p>/info, /api/persons, /api/persons/:id</p>'
+  )
 })
 
 app.get('/api/persons', (request, response) => {
-  Person
-    .find({})
-    .then(result => {
-      if (result) {
-        response.json(result.map(Person.format))
-      }
-      response.status(404).end()
-    })
+  Person.find({}).then(result => {
+    if (result) {
+      response.json(result.map(Person.format))
+    }
+    response.status(404).end()
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  Person
-    .findById(id)
+  Person.findById(id)
     .then(person => {
       response.json(Person.format(person))
     })
-    .catch( () => {
+    .catch(() => {
       //console.log(error)
       response.status(404).end()
     })
@@ -43,25 +47,26 @@ app.get('/api/persons/:id', (request, response) => {
 app.get('/info', (request, response) => {
   //console.log('time', Date.UTC())
   const now = new Date()
-  Person
-    .find({})
-    .then(result => {
-      if (result) {
-        response.send(`<p>puhelinluettelossa ${result.length} henkilön tiedot</p><br/> ${now.toString()}`)
-      }
-      response.status(404).end()
-    })
+  Person.find({}).then(result => {
+    if (result) {
+      response.send(
+        `<p>puhelinluettelossa ${
+          result.length
+        } henkilön tiedot</p><br/> ${now.toString()}`
+      )
+    }
+    response.status(404).end()
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
 
-  Person
-    .findByIdAndRemove(id)
-    .then( () => {
+  Person.findByIdAndRemove(id)
+    .then(() => {
       response.status(204).end()
     })
-    .catch( () => {
+    .catch(() => {
       response.status(400).send({ error: 'malformatted id' })
     })
 })
@@ -76,28 +81,28 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ error: 'number missing' })
   }
 
-  Person
-    .find({ name: body.name })
-    .then(result => {
-      console.log('etsintää', result)
-      if (result.length === 0) {
-        const person = new Person({
-          name: body.name,
-          number: body.number
+  Person.find({ name: body.name }).then(result => {
+    console.log('etsintää', result)
+    if (result.length === 0) {
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+      person
+        .save()
+        .then(savedPerson => {
+          response.json(Person.format(savedPerson))
         })
-        person
-          .save()
-          .then(savedPerson => {
-            response.json(Person.format(savedPerson))
-          })
-          .catch( () => {
-            console.log('error in saving')
-            response.status(400).json({ error: 'database error' })
-          })
-      } else {
-        return response.status(409).json({ error: 'database error, exits already' })
-      }
-    })
+        .catch(() => {
+          console.log('error in saving')
+          response.status(400).json({ error: 'database error' })
+        })
+    } else {
+      return response
+        .status(409)
+        .json({ error: 'database error, exits already' })
+    }
+  })
 })
 
 app.put('/api/persons/:id', (request, response) => {
@@ -110,8 +115,7 @@ app.put('/api/persons/:id', (request, response) => {
   console.log('person ennen tallennusta', person)
   console.log('body ennen tallennusta', body)
 
-  Person
-    .findOneAndUpdate({ _id: request.params.id }, person, { new: true })
+  Person.findOneAndUpdate({ _id: request.params.id }, person, { new: true })
     .then(updatedPerson => {
       console.log('palasi', updatedPerson)
       response.json(Person.format(updatedPerson))
--- osa4/utils/config.js
+++ osa4/utils/config.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+console.log('process.env', process.env.NODE_ENV)
 
 let port = process.env.PORT
 let mongoUrl = process.env.BLOG_DATABASE
